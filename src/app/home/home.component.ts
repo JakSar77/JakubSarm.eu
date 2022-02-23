9c439bd4b7d30ccef833f717d99876e284afe418
Jakub Šarm <JakubSarm@post.cz>
2022-02-23 09:36:51 +0100

    Menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,58 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+    styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+    public citat: any = 'Komerce se vkrádá už i do reklamy...';
 
-    public citat: any   = "Komerce se vkrádá už i do reklamy...";
-
-    public citaty:any   = [
-        "Komerce se vkrádá už i do reklamy...",
-        "Chybovat je lidské, ale něco pořádně podělat,<br>to vyžaduje rootovské heslo.",
-        "Pokud napoprvé neuspějete,<br>nazvěte to „verze 1.0“",
-        "Programátoři jsou nástroje,<br>které zpracovávají kofein na kód.",
-        "Proč chceme inteligentní terminály,<br>když máme takovou spoustu pitomých uživatelů?",
-        "Život by byl mnohem snazší,<br>kdybychom k němu měli zdrojové kódy.",
-        "Můj program nemá chyby.<br>Jen si vyvíjí náhodné funkce.",
-        "Buď v klidu, jsou to jen jedničky a nuly!",
-        "Pokud jste problém nevyřešili silou,<br>pak jste nevyvinuli dostatečnou sílu.",
-        "Nejsem asociál,<br>jsem jen uživatelsky nepřívětivý.",
-        "Pokud nechcete být nahrazen počítačem,<br>nechovejte se jako počítač.",
-        "Lepší být geek než být idiot!",
-        "Internet: místo, kde muži jsou muži, ženy jsou taky muži<br>a děti jsou ve skutečnosti agenti FBI.",
-        "Pokud je debugování proces odstraňování chyb,<br>pak programování musí být vytváření chyb.",
-        "Dnes může počítač používat každý blbec.<br>Mnozí to opravdu dělají.",
-        "Hardware je ta část počítačového systému,<br>kterou lze nakopnout.",
-        "Návrh programovacích jazyků je jako procházka parkem.<br>Konkrétně Jurským parkem.",
-        "Unix je uživatelsky přátelský.<br>Jen si své přátele velmi pečlivě vybírá.",
-        "Jak mám vědět, jestli to bude fungovat?<br>Od toho jsou betatesteři, já to jen píšu!",
-        "Tisíce řádků kvalitního objektového kódu...",
-        "Většina dobrých programátorů neprogramuje proto,<br>že chtějí spoustu peněz a slávu, ale protože je to zábava.",
-        "Mě nezajímá, že to na vašem počítači funguje. My neprodáváme váš počítač!",
-        "Pozor na chyby v tom kódu.<br>Já jsem pouze dokázal, že funguje, nezkoušel jsem ho.",
-        "Teoreticky je teorie a praxe totéž. V praxi tomu tak není.",
-        "Dobrý návrh: hodnota roste rychleji než náklady.",
-        "Chodit po vodě a psát software podle specifikace je snazší, pokud je obojí zmražené.",
-        "Programování je dnes závod mezi softwarovými vývojáři,<br>co se snaží vytvořit větší a lepší blbuvzdorné programy,<br>a vesmírem, který se snaží vytvořit větší a lepší blby. Zatím Vesmír vítězí.",
-        "Ti, co nechápou Linux, jsou odsouzeni k tomu, aby jej vynalezli znovu.",
+    public citaty: any = [
+        'Komerce se vkrádá už i do reklamy...',
+        'Chybovat je lidské, ale něco pořádně podělat,<br>to vyžaduje rootovské heslo.',
+        'Pokud napoprvé neuspějete,<br>nazvěte to „verze 1.0“',
+        'Programátoři jsou nástroje,<br>které zpracovávají kofein na kód.',
+        'Proč chceme inteligentní terminály,<br>když máme takovou spoustu pitomých uživatelů?',
+        'Život by byl mnohem snazší,<br>kdybychom k němu měli zdrojové kódy.',
+        'Můj program nemá chyby.<br>Jen si vyvíjí náhodné funkce.',
+        'Buď v klidu, jsou to jen jedničky a nuly!',
+        'Pokud jste problém nevyřešili silou,<br>pak jste nevyvinuli dostatečnou sílu.',
+        'Nejsem asociál,<br>jsem jen uživatelsky nepřívětivý.',
+        'Pokud nechcete být nahrazen počítačem,<br>nechovejte se jako počítač.',
+        'Lepší být geek než být idiot!',
+        'Internet: místo, kde muži jsou muži, ženy jsou taky muži<br>a děti jsou ve skutečnosti agenti FBI.',
+        'Pokud je debugování proces odstraňování chyb,<br>pak programování musí být vytváření chyb.',
+        'Dnes může počítač používat každý blbec.<br>Mnozí to opravdu dělají.',
+        'Hardware je ta část počítačového systému,<br>kterou lze nakopnout.',
+        'Návrh programovacích jazyků je jako procházka parkem.<br>Konkrétně Jurským parkem.',
+        'Unix je uživatelsky přátelský.<br>Jen si své přátele velmi pečlivě vybírá.',
+        'Jak mám vědět, jestli to bude fungovat?<br>Od toho jsou betatesteři, já to jen píšu!',
+        'Tisíce řádků kvalitního objektového kódu...',
+        'Většina dobrých programátorů neprogramuje proto,<br>že chtějí spoustu peněz a slávu, ale protože je to zábava.',
+        'Mě nezajímá, že to na vašem počítači funguje. My neprodáváme váš počítač!',
+        'Pozor na chyby v tom kódu.<br>Já jsem pouze dokázal, že funguje, nezkoušel jsem ho.',
+        'Teoreticky je teorie a praxe totéž. V praxi tomu tak není.',
+        'Dobrý návrh: hodnota roste rychleji než náklady.',
+        'Chodit po vodě a psát software podle specifikace je snazší, pokud je obojí zmražené.',
+        'Programování je dnes závod mezi softwarovými vývojáři,<br>co se snaží vytvořit větší a lepší blbuvzdorné programy,<br>a vesmírem, který se snaží vytvořit větší a lepší blby. Zatím Vesmír vítězí.',
+        'Ti, co nechápou Linux, jsou odsouzeni k tomu, aby jej vynalezli znovu.',
     ];
 
     constructor() {
-        this.citat  = this.citaty[Math.floor(Math.random() * this.citaty.length)];
+        this.citat = this.citaty[Math.floor(Math.random() * this.citaty.length)];
     }
 
     ngOnInit(): void {
-        window.onblur   = function(){
-            document.title  = "Nezapomněli jste na mě? | Jakub Šarm";
+        window.onblur = function () {
+            document.title = 'Nezapomněli jste na mě? | Jakub Šarm';
         };
-        window.onfocus   = function(){
-            document.title  = "Jakub Šarm - Programátor";
+        window.onfocus = function () {
+            document.title = 'Jakub Šarm - Programátor';
         };
     }
 
 
+    public mLink: any = "main";
+    menu(link:any){
+        this.mLink   = link;
+        link.scrollIntoView();
+    }
+
+    @HostListener('window:scroll', ['$event']) 
+    scrollHandler(event:any) {
+        console.debug("Scroll Event");
+    }
 
 }
